d159efa0a6e07cc8da738154d934e965dbca9526
havvk01 <havvk01@gmail.com>
2022-09-11 19:20:45 +0300

    Initial Commit

---
+++ CoreDataDemo_CWC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo_CWC
+//
+//  Created by Slava Havvk on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
