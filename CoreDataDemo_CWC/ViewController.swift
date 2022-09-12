373f0c44c0c3c6879342a0cbbf5b5bc780545a7c
havvk01 <havvk01@gmail.com>
2022-09-12 21:15:50 +0300

    add coreData and tableView

--- CoreDataDemo_CWC/ViewController.swift
+++ CoreDataDemo_CWC/ViewController.swift
@@ -6,14 +6,166 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    
+    var items:[Person]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchPeople()
+//        (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
+    func fetchPeople() {
+        do {
+            self.items = try context.fetch(Person.fetchRequest())
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        catch {
+            
+        }
     }
 
 
+    @IBAction func addTapped(_ sender: Any) {
+        
+        // Create alert
+        let alert = UIAlertController(title: "Add Person", message: "What is their name?", preferredStyle: .alert)
+        alert.addTextField()
+        
+        // Configure button handler
+        let submitButton = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            // Get the textfield for the alert
+            let textfield = alert.textFields![0]
+            
+            // Create a person object
+            let newPerson = Person(context: self.context)
+            newPerson.name = textfield.text
+            newPerson.age = 22
+            newPerson.gender = "Male"
+            
+            // Save the data
+            do {
+            try self.context.save()
+            }
+            catch {
+                
+            }
+            
+            
+            // Re-fetch the data
+            self.fetchPeople()
+        }
+        
+        // Add button
+        alert.addAction(submitButton)
+        
+        // Show alert
+        self.present(alert, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Return the number of people
+        return self.items?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
+        
+        
+        // Get the person from array and set the label
+        let person = self.items![indexPath.row]
+        cell.textLabel?.text = person.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Selected person
+        let person = self.items![indexPath.row]
+        
+        // Create alert
+        let alert = UIAlertController(title: "Edit Person", message: "Edit name", preferredStyle: .alert)
+        alert.addTextField()
+        
+        let textfield = alert.textFields![0]
+        textfield.text = person.name
+        
+        // Configure button handler
+        let saveButton = UIAlertAction(title: "Save", style: .default) { (action) in
+            
+            // Get the textfield for the alert
+            let textfield = alert.textFields![0]
+            
+            // Edit name property of person object
+            person.name = textfield.text
+            
+            // TODO: Save the data
+            do {
+                try self.context.save()
+            }
+            catch {
+                
+            }
+            
+            // TODO: Re-fetch the data
+            self.fetchPeople()
+            
+        }
+        
+        // Add button
+        alert.addAction(saveButton)
+        
+        // Show alert
+        self.present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        // Create swipe action
+        let action = UIContextualAction(style: .destructive, title: "Delete") { action, view, completionHandler in
+            
+            // Which person to remove
+            let personToRemove = self.items![indexPath.row]
+            
+            // Remove the person
+            self.context.delete(personToRemove)
+            
+            // Save the data
+            do {
+                try self.context.save()
+            }
+            catch {
+                
+            }
+            
+            // Re-fetch the data
+            self.fetchPeople()
+        }
+        
+        //return swipe action
+        return UISwipeActionsConfiguration(actions: [action])
+    }
 }
 
---
+++ Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo_CWC
+//
+//  Created by Slava Havvk on 12.09.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
